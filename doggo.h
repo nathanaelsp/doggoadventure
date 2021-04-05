293cae1324024ffc097f7f565095dec9833e4f07
rictoes <51188489+rictoes@users.noreply.github.com>
2021-04-05 23:56:34 +0800

    Update doggo.h

--- doggo.h
+++ doggo.h
@@ -2,52 +2,98 @@
 
 extern int doggo_face;
 extern int doggo_dir;
+extern int doggo_dir_next;
 extern int doggo_x;
 extern int doggo_y;
-extern int doggo_x_target;
-extern int doggo_y_target;
+extern int doggo_pathmap_x;
+extern int doggo_pathmap_y;
 extern int doggo_speed;
+extern int doggo_intersect;
 int counter = 0;
+
+void doggo_move(void)
+{
+	//move down
+	if(doggo_dir == 0){
+		if(doggo_intersect && pathmap[doggo_pathmap_y+1][doggo_pathmap_x] == 1) doggo_pathmap_y++;
+		if(doggo_y < doggo_pathmap_y*8) doggo_y += doggo_speed;
+	}
+	//move up
+	if(doggo_dir == 1){
+		if(doggo_intersect && pathmap[doggo_pathmap_y-1][doggo_pathmap_x] == 1) doggo_pathmap_y--;
+		if(doggo_y > doggo_pathmap_y*8) doggo_y -= doggo_speed;
+	}
+	//move right
+	if(doggo_dir == 2){
+		if(doggo_intersect && pathmap[doggo_pathmap_y][doggo_pathmap_x+1] == 1) doggo_pathmap_x++;
+		if(doggo_x < doggo_pathmap_x*8) doggo_x += doggo_speed;
+	}
+	//move left
+	if(doggo_dir == 3){
+		if(doggo_intersect && pathmap[doggo_pathmap_y][doggo_pathmap_x-1] == 1) doggo_pathmap_x--;
+		if(doggo_x > doggo_pathmap_x*8) doggo_x -= doggo_speed;
+	}
+}
 
 //{0,1,2,3} = {down,up,right,left}
 void drawDoggo_pathmap(void)
 {
-	//int pathmap_dest = doggo_x/8 + 28*(doggo_y/8-1);
+	doggo_intersect = 0; //reset intersect
+	if(doggo_y == doggo_pathmap_y*8 && doggo_x == doggo_pathmap_x*8)  doggo_intersect = 1; //check if intersect
 
-	int pathmap_x;
-	int pathmap_y;
-	
-	switch(doggo_dir){
-	case 0: //down
-	pathmap_x = doggo_x/8;
-	pathmap_y = doggo_y/8;
-	if(pathmap[pathmap_y+1][pathmap_x] == 1)
-		doggo_y++;
-	break;
-	
-	case 1: //up
-	pathmap_x = doggo_x/8;
-	pathmap_y = (doggo_y+7)/8;
-	if(pathmap[pathmap_y-1][pathmap_x] == 1)
-		doggo_y--;
-	break;
-
-	case 2: //right
-	pathmap_x = doggo_x/8;
-	pathmap_y = doggo_y/8;
-	if(pathmap[pathmap_y][pathmap_x+1] == 1)
-		doggo_x++;
-	break;
-
-	case 3: //left
-	pathmap_x = (doggo_x+7)/8;
-	pathmap_y = doggo_y/8;
-	if(pathmap[pathmap_y][pathmap_x-1] == 1)
-		doggo_x--;
-	break;
+	//not intersect
+	if(doggo_intersect == 0){
+		//currently moving down and up is pressed, face up
+		if(doggo_dir == 0 && doggo_dir_next == 1){
+			doggo_dir = 1;
+			doggo_pathmap_y--;
+		}
+		//currently moving up and down is pressed, move down
+		if(doggo_dir == 1 && doggo_dir_next == 0){
+			doggo_dir = 0;
+			doggo_pathmap_y++;
+		}
+		//currently moving right and left is pressed, move left
+		if(doggo_dir == 2 && doggo_dir_next == 3){
+			doggo_dir = 3;
+			doggo_pathmap_x--;
+		}
+		//currently moving left and right is pressed, move right
+		if(doggo_dir == 3 && doggo_dir_next == 2){
+			doggo_dir = 2;
+			doggo_pathmap_x++;
+		}
+		//move in current direction
+		doggo_move();
 	}
 
-	//draw sprite
+	//intersect
+	if(doggo_intersect == 1){
+		//down has been pressed & tile below is empty, move down
+		if(doggo_dir_next == 0 && pathmap[doggo_pathmap_y+1][doggo_pathmap_x] == 1){
+			doggo_pathmap_y++;
+			doggo_dir = 0;
+		}
+		//up has been pressed & tile above is empty, move down
+		else if(doggo_dir_next == 1 && pathmap[doggo_pathmap_y-1][doggo_pathmap_x] == 1){
+			doggo_pathmap_y--;
+			doggo_dir = 1;
+		}
+		//right has been pressed & tile to the right is empty, move right
+		else if(doggo_dir_next == 2 && pathmap[doggo_pathmap_y][doggo_pathmap_x+1] == 1){
+			doggo_pathmap_x++;
+			doggo_dir = 2;
+		}
+		//left has been pressed & tile to the left is empty, move left
+		else if(doggo_dir_next == 3 && pathmap[doggo_pathmap_y][doggo_pathmap_x-1] == 1){
+			doggo_pathmap_x--;
+			doggo_dir = 3;
+		}
+		//continue moving in current direction
+		doggo_move();
+	}
+
+	//draw doggo
 	if(counter == 0)
 	{
 		if(doggo_face == doggo_down_close) doggo_face++;
@@ -55,7 +101,7 @@ void drawDoggo_pathmap(void)
 	}
 	counter++;
 	counter = counter%5;
-	drawSprite(doggo_face + doggo_dir*2, 0, doggo_x+8, doggo_y+8);
+	drawSprite(doggo_face + doggo_dir*2, 0, doggo_x, doggo_y);
 }
 
 void drawDoggo(void)
